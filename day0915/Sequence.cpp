a5c7e0dec5223e901d77e412952b5dd095f4ea02
cjsoft <egwcyh@qq.com>
2015-09-15 19:23:56 +0800

    n^5 with bits

--- day0915/Sequence.cpp
+++ day0915/Sequence.cpp
@@ -18,7 +18,7 @@ int main(){
             for (int k = j+1; k <= n-2; k ++) {
                 for (int l = k+1; l <= n-1; l++) {
                     for(int m=l+1;m<=n;m++){
-                        if((a[j]==a[l] && a[k]==a[m]) || dp[i]%4==0){
+                        if(!(((a[j]^a[l]) || (a[k]^a[m])) && (dp[i]&4))){
                             dp[j]=max(dp[j],dp[i]+1);
                             dp[k]=max(dp[k],dp[i]+2);
                             dp[l]=max(dp[l],dp[i]+3);
