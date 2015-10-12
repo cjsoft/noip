a1ab58e364aea782928f0a77751d3895abbc6402
CJSoft <egwcyh@qq.com>
2015-10-12 20:00:09 +0800

    lalalala

--- day1012/city.cpp
+++ day1012/city.cpp
@@ -2,35 +2,41 @@
 #include <cstdio>
 #include <vector>
 #include <memory.h>
+
 typedef long long ll;
 const ll MOD = 1000000007;
 
 using namespace std;
 
 ll dp[200007];
-ll jiecheng[200007];
+
 
 int n;
 vector <int> mp[200007];
 
-void init_jiecheng(){
-	jiecheng[0]=1;
-	for (int i = 1; i < 200007; ++i){
-		jiecheng[i]=jiecheng[i-1]*i;
-		jiecheng[i]%=MOD;
+void dfs(int root,int from){
+	if(mp[root].size()==1 || 1){
+		dp[root]=1;
+		// return;
 	}
+	for (vector<int>::iterator i = mp[root].begin(); i != mp[root].end(); ++i)
+	{
+		if(*i!=from)
+			dfs(*i,root);
+	}
+	for (vector<int>::iterator i = mp[root].begin(); i != mp[root].end(); ++i)
+	{
+		if(*i!=from){
+			dp[root]*=dp[*i]+1;
+			dp[root]%=MOD;
+		}
+	}
+	// dp[root]++;
+	dp[root]%=MOD;
 }
-
-
-ll select_C(int a,int in_b){
-	return (jiecheng[in_b]/jiecheng[a]/jiecheng[in_b-a]);
-}
-
-
 
 int main(){
 	ios::sync_with_stdio(false);
-	init_jiecheng();
 	cin>>n;
 	int temp;
 	for (int i = 2; i <=n; ++i){
@@ -38,6 +44,11 @@ int main(){
 		mp[i].push_back(temp);
 		mp[temp].push_back(i);
 	}
-
+	for (int i = 1; i <=n; ++i)
+	{
+		memset(dp,0,sizeof(dp));
+		dfs(i,0);
+		cout<<dp[i]<<endl;
+	}
 	return 0;
 }
