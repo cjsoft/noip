5e36dc9fc282e3fd07a21b2d87873d789f3cc006
CJSoft <egwcyh@qq.com>
2015-10-14 19:04:29 +0800

    min.cpp on day 1014

---
+++ day1014/min.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <algorithm>
+
+using namespace std;
+const int POI=1000007;
+int a[POI];
+int b[POI];
+int n;
+
+template <typename T>
+T mabs(T a){
+	return a<0?-a:a;
+}
+
+int main(){
+	scanf("%d",&n);
+	for (int i = 0; i <n; ++i)
+	{
+		scanf("%d",a+i);
+		a[i]=mabs(a[i]);
+	}
+	sort(a,a+n);
+	int minn=INT_MAX;
+	for (int i = 0; i < n; ++i)
+	{
+		if(a[i]!=0)
+			minn=min(minn,a[i]);
+	}
+	for (int i = 0; i < n-1; ++i)
+	{
+		if(a[i+1]%a[i]!=0)
+			minn=min(mabs(a[i+1]%a[i]),minn);
+		if(a[i+1]-a[i]!=0)
+			minn=min(mabs(a[i+1]-a[i]),minn);
+	}
+	printf("%d\n",minn );
+	return 0;
+}
