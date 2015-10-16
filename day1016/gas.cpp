628f852931c276e073041b17c38b63639e744bdb
CJSoft <egwcyh@qq.com>
2015-10-16 18:31:02 +0800

    gas modified

--- day1016/gas.cpp
+++ day1016/gas.cpp
@@ -17,6 +17,12 @@ int main(){
         scanf("%d",b+i);
     }
     double avg=double(total)/n;
-    
+    for (int i = 0; i < n; ++i) {
+        if(avg>b[i]){
+            printf("TvT\n");
+            return 0;
+        }
+    }
+    printf("%.12lf\n",avg );
     return 0;
 }
