588bf82c920693bdfa92589207a74f3366ef3f78
CJSoft <egwcyh@qq.com>
2015-10-16 12:04:41 +0000

    sss

--- day1016/gas.cpp
+++ day1016/gas.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 #include <cstdio>
+#define open(a,b) freopen(a,"r",stdin);freopen(b,"w",stdout);
 
 using namespace std;
 typedef long long ll;
 const int CNT=1000007;
 int n;
-int a[CNT],b[CNT];
+ll a[CNT],b[CNT];
 ll total=0;
 int main(){
+    open("gas.in","gas.out");
     scanf("%d",&n);
     for (int i = 0; i < n; i++) {
-        scanf("%d",a+i);
+        scanf("%lld",a+i);
         total+=a[i];
     }
     for (int i = 0; i < n; i++) {
-        scanf("%d",b+i);
+        scanf("%lld",b+i);
     }
     double avg=double(total)/n;
     for (int i = 0; i < n; ++i) {
