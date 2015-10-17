eedb7fb702e34980bd8a1795e85de2980395f15f
CJSoft <egwcyh@qq.com>
2015-10-17 12:07:44 +0800

    tijie

--- day1017/test_generator.py
+++ day1017/test_generator.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 import sys
 print "todo list",sys.argv[1:]
 for i in sys.argv[1:]:
@@ -8,4 +9,21 @@ for i in sys.argv[1:]:
 		k=random.randint(0,n)
 		s=random.randint(0,int(0.5*(n-k+1+n)*k))
 		f.write("%d %d %d\n"%(n,k,s))
-		print "number done"
+		f.close()
+		print "%s done"%i
+	elif(i=="circle"):
+		import random
+		f=open("circle.in","w")
+		n=random.randint(1,300000)
+		f.write("%d\n"%n)
+		for j in range(n):
+			x=random.randint(-999999999,999999999)
+			r=random.randint(1,1000000000-abs(x))
+			f.write("%d %d\n"%(x,r))
+		f.close()
+		print "%s done"%i
+	elif(i=="wander"):
+		f=open("wander.in","w")
+		f.write("我不喜欢wander...")
+		f.close()
+		print "%s done"%i
