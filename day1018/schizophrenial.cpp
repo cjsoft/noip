e6d284082919226fdada7312b3139ddee765deb3
CJSoft <egwcyh@qq.com>
2015-10-18 02:39:33 +0000

    to local

--- day1018/schizophrenial.cpp
+++ day1018/schizophrenial.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 #include <cstdio>
-#include <queue>
+#include <cstring>
 
 using namespace std;
+int x,l,r,p;
+bool tags[1000007];
+void dfs(int a,int b,int tms){
+    if(tms>p || a>r)return ;
+    tags[a]=true;
+    dfs(a,b+1,tms+1);
+    dfs(a*b,b,tms+1);
+}
 
 int main(){
-
-	return 0;
+    memset(tags,0,sizeof(tags));
+    cin>>l>>r>>p;
+    dfs(1,0,0);
+    int cnt=0;
+    for (int i = l; i <=r; i++) {
+        if(tags[i])++cnt;
+    }
+    cout<<cnt;
+    return 0;
 }
---
+++ day1018/throne.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#define open(a,b) freopen(a,"r",stdin);freopen(b,"w",stdout)
+using namespace std;
+
+struct wangshichengyuan{
+  char gender;
+  char cnthoudai;
+  int houdaiid[6];
+};
+
+int main(){
+    cout<<"2"<<endl;
+    return 0;
+}
