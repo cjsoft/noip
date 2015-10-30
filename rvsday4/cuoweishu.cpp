263ce30feca745c2ecaa0309311e255f21e454c1
CJSoft <egwcyh@qq.com>
2015-10-30 22:17:56 +0800

    cuoweishu n^2

--- rvsday4/cuoweishu.cpp
+++ rvsday4/cuoweishu.cpp
@@ -1,31 +1,34 @@
+// 这道题其实可以打表（错位数） （雾）
 #include <iostream>
 #include <cstdio>
 
 using namespace std;
 typedef long long ll;
-ll jiecheng[57];
-ll qes[57];
+ll jiecheng[21];
+ll qes[21];
 ll C(int a,int b){
+	printf("%d %d\n",a,b);
+	// return 1;
 	return jiecheng[a]/jiecheng[b]/jiecheng[a-b];
 }
 
 int main(){
 	printf("safa");
 	jiecheng[1]=1;
-	for (int i=2;i<57;i++){
+	for (int i=2;i<21;i++){
 		jiecheng[i]=jiecheng[i-1]*i;
 	}
 	qes[1]=0;
 	qes[2]=1;
-	for(int i=3;i<57;++i){
+	for(int i=3;i<21;++i){
 		qes[i]=jiecheng[i]-1;
-		for(int j=1;j<=i-1;++i){
+		for(int j=1;j<=i-1;++j){
 			qes[i]-=C(i,j)*qes[i-j];
 
 		}
 	}
-//	for(int i=0;i<57;++i)
-//		printf("%d %lld",i,qes[i]);
+	for(int i=0;i<21;++i)
+		printf("%d %lld\n",i,qes[i]);
 	return 0;
 }
 
