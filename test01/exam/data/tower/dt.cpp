ef23a22464fb50593ffaf1e0cd4f0ab254a3ef54
CJSoft <egwcyh@qq.com>
2015-11-01 11:39:18 +0800

    solu

---
+++ test01/exam/data/tower/dt.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pr;
+const double pi=acos(-1);
+#define rep(i,a,n) for(int i=a;i<=n;i++)
+#define per(i,n,a) for(int i=n;i>=a;i--)
+#define Rep(i,u) for(int i=head[u];i;i=Next[i])
+#define clr(a) memset(a,0,sizeof a)
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define sc second
+ld eps=1e-9;
+ll pp=1000000007;
+ll mo(ll a,ll pp){if(a>=0 && a<pp)return a;a%=pp;if(a<0)a+=pp;return a;}
+ll powmod(ll a,ll b,ll pp){ll ans=1;for(;b;b>>=1,a=mo(a*a,pp))if(b&1)ans=mo(ans*a,pp);return ans;}
+ll read(){
+	ll ans=0;
+	char last=' ',ch=getchar();
+	while(ch<'0' || ch>'9')last=ch,ch=getchar();
+	while(ch>='0' && ch<='9')ans=ans*10+ch-'0',ch=getchar();
+	if(last=='-')ans=-ans;
+	return ans;
+}
+//head
+int main(){
+	freopen("tower10.in","w",stdout);
+	srand((unsigned)time(NULL));
+	int n=100;
+	printf("%d\n",n);
+	rep(i,1,n-1)
+		rep(j,i+1,n)
+			if(n==j)printf("%d\n",1+rand()%10000);
+			else printf("%d ",1+rand()%10000);
+	return 0;
+}
