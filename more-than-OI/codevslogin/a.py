69661be07a5efacc90700b157d5bf84696eba3f4
CJSoft <egwcyh@qq.com>
2015-12-07 21:16:17 +0800

    ;local codevs

--- more-than-OI/codevslogin/a.py
+++ more-than-OI/codevslogin/a.py
@@ -1,10 +1,12 @@
 #coding=utf-8
 import urllib2, cookielib
 from urllib import urlencode
-import sys, re
+import sys, re, os
+from bs4 import BeautifulSoup
 reload(sys)
 sys.setdefaultencoding('utf-8')
 last_red = ''
+DMN = 'http://codevs.cn'
 class RedirectHandler(urllib2.HTTPRedirectHandler):
     def http_error_301(self, req, fp, code, msg, headers):
         global last_red
@@ -18,15 +20,25 @@ class RedirectHandler(urllib2.HTTPRedirectHandler):
         # print headers
 r = re.compile(r"csrfmiddlewaretoken: '([a-zA-Z0-9]*)'")
 r2 = re.compile(r'Location: (http://[a-zA-Z\.0-9/\-\?\&\+\=]*)')
+r3 = re.compile(r'/(\d{4})/')
+r4 = {
+    'AC':re.compile(r'\'AC\', data: (\d*) '),
+    'WA':re.compile(r'\'WA\', data: (\d*) '),
+    'TLE':re.compile(r'\'TLE\', data: (\d*) '),
+    'MLE':re.compile(r'\'MLE\', data: (\d*) '),
+    'RE':re.compile(r'\'RE\', data: (\d*) ')
+}
+r5 = re.compile(r'题目等级 :   (青铜|白银|黄金|钻石|大师) ')
+r6 = re.compile(r'时间限制: (\d{1,2}) s')
+r7 = re.compile(r'空间限制: (\d{1,7}) KB')
 cookie = cookielib.CookieJar()
 handler = urllib2.HTTPCookieProcessor(cookie)
-opener = urllib2.build_opener(handler)
-opener.open('http://www.baidu.com')
+opener = urllib2.build_opener(handler)#, urllib2.ProxyHandler({'http':'http://192.168.0.201:808'}))
 def getUnRedirectUrl(url,timeout=10):
     global handler
     req = urllib2.Request(url)
     debug_handler = urllib2.HTTPHandler()
-    opener = urllib2.build_opener(debug_handler, RedirectHandler, handler)
+    opener = urllib2.build_opener(debug_handler, RedirectHandler, handler)#, urllib2.ProxyHandler({'http':'http://192.168.0.201:808'}))
     
     html = None
     response = None
@@ -47,7 +59,6 @@ def getUnRedirectUrl(url,timeout=10):
         return html
     else:
         return error_info
-opener = urllib2.build_opener(handler)
 url = r'http://account.codevs.com/api/user/login/'
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36'
 headers = { 
@@ -64,21 +75,20 @@ resp = opener.open('http://account.codevs.com/user/login/')
 for j in cookie:
     s = j.value
 
-print s
 values = {
 'username':'cjsoft',
-'password':'sadwqre',
+'password':'safsdf',
 'csrfmiddlewaretoken':s,
 'next':'',
 }
-def make_cookie(name, value):
+def make_cookie(name, value, dimain):
     return cookielib.Cookie(
         version=0,
         name=name,
         value=value,
         port=None,
         port_specified=False,
-        domain="account.codevs.com",
+        domain=dimain,
         domain_specified=True,
         domain_initial_dot=False,
         path="/",
@@ -92,7 +102,7 @@ def make_cookie(name, value):
     )
 def csrf(a):
     global cookie
-    print r.findall(a)[0]
+    # print r.findall(a)[0]
     cookie.set_cookie(make_cookie('csrftoken',r.findall(a)[0]))
 def locate(a):
     global last_red, opener
@@ -106,7 +116,7 @@ def locate(a):
     # print last_red,'sad'
     # print r2.findall(str(getUnRedirectUrl(a)))
 postdata = urlencode(values)
-print cookie
+# print cookie
 # login
 req = urllib2.Request(url, postdata, headers)
 resp = opener.open(req)
@@ -119,7 +129,68 @@ f= open('adsf.txt', 'w')
 # f.write(a)
 # csrf(a)
 # print cookie
+def fetch(link, path, codeid):
+    try:
+        print link, path, codeid
+        resp = locate(link)
+        if (str(resp).find('题目审核未通过或正在审核中') != -1):
+            return
+        # if int(codeid) == 1030:
+        #     f=open('D:\\a.txt','w')
+        #     f.write(str(resp))
+        #     f.close()
+        resp = resp.replace('<br>', 'fnadjsihfinadsiufhasdubfueabfuwebeu')
+        soup =BeautifulSoup(resp, 'html.parser', from_encoding = 'utf-8')
+        pinfo = soup.find('div', class_='col-lg-9 no-padder ').find_all('div',class_='panel panel-default')
+        f = open(path + '\\%s.md'%codeid, 'w')
+        n1 = soup.find('h3', class_='m-t m-b-sm', style='display:inline-block').get_text() # class="m-t m-b-sm" style="display:inline-block"
+        # print r6.findall(resp)[0],r7.findall(resp)[0],r5.findall(resp)[0]
+        f.write('#%s  \n'%n1)
+        f.write('### 时间限制： %s s     空间限制： %s KB     题目等级： %s  \n'%(
+            r6.findall(resp)[0],
+            r7.findall(resp)[0],
+            r5.findall(resp)[0]
+            ))
+        # print pinfo
+        # for i in pinfo:
+        #     print i, '\n'
+        f.write('## 题目描述：  \n\n<pre>%s</pre>\n  \n  \n'%pinfo[0].find('div',class_='panel-body').get_text().replace('fnadjsihfinadsiufhasdubfueabfuwebeu','  \n'))
+        f.write('## 输入描述：  \n\n<pre>%s</pre>\n  \n  \n'%pinfo[1].find('div',class_='panel-body').get_text().replace('fnadjsihfinadsiufhasdubfueabfuwebeu','  \n'))
+        f.write('## 输出描述：  \n\n<pre>%s</pre>\n  \n  \n'%pinfo[2].find('div',class_='panel-body').get_text().replace('fnadjsihfinadsiufhasdubfueabfuwebeu','  \n'))
+        f.write('## 样例输入：  \n\n<pre><code>%s</code></pre>\n  \n  \n'%pinfo[3].find('div',class_='panel-body').get_text().replace('fnadjsihfinadsiufhasdubfueabfuwebeu','  \n'))
+        f.write('## 输出输出：  \n\n<pre><code>%s</code></pre>\n  \n  \n'%pinfo[4].find('div',class_='panel-body').get_text().replace('fnadjsihfinadsiufhasdubfueabfuwebeu','  \n'))
+        f.write('## 数据范围及提示：  \n\n<pre>%s</pre>\n  \n  \n'%pinfo[5].find('div',class_='panel-body').get_text().replace('fnadjsihfinadsiufhasdubfueabfuwebeu','  \n'))
+        f.write('***  \n\n##### AC: %s  \n'%r4['AC'].findall(resp)[0])
+        f.write('##### WA: %s  \n'%r4['WA'].findall(resp)[0])
+        f.write('##### TLE: %s  \n'%r4['TLE'].findall(resp)[0])
+        f.write('##### MLE: %s  \n'%r4['MLE'].findall(resp)[0])
+        f.write('##### RE: %s  \n'%r4['RE'].findall(resp)[0])
+        # print n
+        f.close()
+    except BaseException:
+        pass
 
-resp = locate('http://codevs.cn/submission/?ismine=1')
-f.write(resp)
+for i in xrange(0):
+    resp = locate('http://codevs.cn/problem/?problemset_id=1&page=%s'%i)
+    # print resp
+    soup = BeautifulSoup(resp, 'html.parser', from_encoding = 'utf-8')
+    sss = str(soup.find('tbody'))
+    ssp = BeautifulSoup(sss, 'html.parser', from_encoding = 'utf-8')
+    # print type(ssp)
+    coll = ssp.find_all('a', class_ = 'pull-left', title= '')
+    for j in coll:
+        # print (j.string).encode('gb18030'), j['href']
+        codeid = str(r3.findall(j['href'])[0])
+        print codeid
+        if (not os.path.exists('problems\\'+codeid)):
+            os.makedirs(r'problems\\'+codeid)
+        fetch(DMN + r'/problem/' + codeid + '/', 'problems\\'+codeid, codeid)
+        print 'fetched', codeid
+        # break
+    # break
+for codeid in xrange(1000, 5000):
+    if (not os.path.exists('problems\\')):
+        os.makedirs(r'problems\\')
+    fetch(DMN + r'/problem/' + str(codeid) + '/', 'problems\\', str(codeid))
+    print 'fetched', str(codeid)
 f.close()
