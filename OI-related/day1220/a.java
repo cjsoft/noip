544c7d6eeb195758459ee501fede8f644d17144d
CJSoft <egwcyh@qq.com>
2015-12-20 21:50:24 +0800

    ;some useless test code

---
+++ OI-related/day1220/a.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+class I {
+    I() {}
+    public String toString() {
+        return "123";
+    }
+}
+public class a {
+    public static void main(String []args) {
+        System.out.println(new I());
+    }
+}
