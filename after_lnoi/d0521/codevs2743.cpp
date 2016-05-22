2829274a64d0d291a247e333c439b882f8cefb9d
CJSoft <egwcyh@qq.com>
2016-05-22 22:12:53 +0800

    iQEcBAABAgAGBQJXQb5lAAoJEIkVSVwbGBpgQ1YH/jqtvNQ/BzxhrLU8ndxluxm2  kEcKa4p2hyXYHiqd7A3ZyfG8NcTvE7IbXP6itvukmdx5MBx5PNUsoLdNAy+Kb/iC  8cc2NePLiuXlEvhlVLPVH0JslkU/eWovNfk+FSCzAke1qj2wHwlGEXTWZVPa2OZx  aJRtzsqhODmROJ3XU/mhzXdXt2cOtcdbfWsnQpaG6i/bb/Mq7SbWUhk7lYfqNW0r  oIemP5awW50v7gwmFmrA22YZjRyYH1tv/TVRgajU6OGpr2F7xn0ytuTz0Eg+uZEm  HVxngzvJgiCgHfggYm40BpRlYYmrdFHysSxd8dA725ZqHlZYV3UbuaVI3ezl+4Y=  =ahyi  -----END PGP SIGNATURE-----
    ;轮廓线dp

--- after_lnoi/d0521/codevs2743.cpp
+++ after_lnoi/d0521/codevs2743.cpp
@@ -4,10 +4,44 @@
 #define pick(a, i) ((a >> (i - 1)) & 1)
 int h, w;
 int dp[13][3007];
-int st[3007];
-
+inline bool chklne1(int stat) {
+    int i = 1;
+    while (i <= w) {
+        if (pick(stat, i)) {
+            if (i == w || !pick(stat, i + 1)) return false;
+            else i += 2;
+        } else ++i;
+    }
+    return true;
+}
+inline validate(int stat1, int stat2) {
+    int i = 1;
+    while (i <= w) {
+        if (pick(stat1, i)) {
+            if (pick(stat2, i)) {
+                if (i == w || !pick(stat1, i + 1) || !pick(stat2, i + 1)) return false;
+                else i += 2;
+            } else ++i;
+        }
+    }
+    return true;
+}
 int main() {
     while (~scanf("%d %d", &h, &w)) {
-        
+        if (!(h && w)) return 0;
+        if (h < w) std::swap(h, w);
+        int mask = (1 << w) - 1;
+        memset(dp, 0, sizeof(dp));
+        for (int i = 0; i <= mask; ++i) {
+            if (chklne1(i)) dp[1][j] = 1;
+        }
+        for (int i = 2; i <= h; ++i) {
+            for (int j = 0; j <= mask; ++j) {
+                for (int k = 0; k <= mask; ++k) {
+                    if (validate(k, j)) dp[i][j] += dp[i - 1][k];
+                }
+            }
+        }
+        printf("%d\n", dp[h][w]);
     }
 }
--- after_lnoi/d0521/poj2411.cpp
+++ after_lnoi/d0521/poj2411.cpp
@@ -1,17 +1,51 @@
 #include <iostream>
 #include <cstdio>
-#include <bitset>
 #include <cstring>
-#define INF 0x3f3f3f3f
-typedef std::bitset<16> BITS;
-int dp[12877];
-int st[]
-char inpt[67];
-
+#define pick(a, i) ((a >> (i - 1)) & 1)
+typedef long long ll;
+int h, w;
+ll dp[13][3007];
+inline bool chklne1(int stat) {
+    int i = 1;
+    while (i <= w) {
+        if (pick(stat, i)) {
+            if (i == w || !pick(stat, i + 1)) return false;
+            else i += 2;
+        } else ++i;
+    }
+    return true;
+}
+inline bool validate(int stat1, int stat2) {
+    int i = 1;
+    while (i <= w) {
+        if (pick(stat1, i)) {
+            if (pick(stat2, i)) {
+                if (i == w || !pick(stat1, i + 1) || !pick(stat2, i + 1)) return false;
+                else i += 2;
+            } else ++i;
+        } else {
+            if (pick(stat2, i)) ++i;
+            else return false;
+        }
+    }
+    return true;
+}
 int main() {
-    for (int i = 0; i < 4; ++i) scanf("%s", inpt + 4 * i);
-    for (int i = 0; i < 65537; ++i) dp[i] = INF;
-    BITS st(std::string(inpt));
-    dp[st.to_ulong()] = 0;
-
+    while (~scanf("%d %d", &h, &w)) {
+        if (!(h && w)) return 0;
+        if (h < w) std::swap(h, w);
+        int mask = (1 << w) - 1;
+        memset(dp, 0, sizeof(dp));
+        for (int i = 0; i <= mask; ++i) {
+            if (chklne1(i)) dp[1][i] = 1;
+        }
+        for (int i = 2; i <= h; ++i) {
+            for (int j = 0; j <= mask; ++j) {
+                for (int k = 0; k <= mask; ++k) {
+                    if (validate(j, k)) dp[i][j] += dp[i - 1][k];
+                }
+            }
+        }
+        printf("%lld\n", dp[h][mask]);
+    }
 }
