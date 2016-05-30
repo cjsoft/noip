9664209519acb0381771746e709d116f3df58664
CJSoft <egwcyh@qq.com>
2016-05-30 23:01:47 +0800

    codevs2460; 树的统计@lct

--- after_lnoi/d0530/codevs2460.cpp
+++ after_lnoi/d0530/codevs2460.cpp
@@ -3,13 +3,21 @@
 #include <cstring>
 #include <algorithm>
 #define MXN 200007
+#define INF 0x3f3f3f3f
 #define isrson(x) (x == x->parent->c[1])
 #define isroot(x) (!(x->parent->c[0] == x || x->parent->c[1] == x))
-struct node {int sz; bool isrev; node *parent, *c[2];} *nil, _nil;
-inline void update(node *x) {x->sz = x->c[0]->sz + x->c[1]->sz + 1;}
+struct node {int sz, sum, mmm, data; bool isrev; node *parent, *c[2];} *nil, _nil;
+inline void update(node *x) {
+    if (x == nil) return;
+    x->sz = x->c[0]->sz + x->c[1]->sz + 1;
+    x->sum = x->c[0]->sum + x->c[1]->sum + x->data;
+    x->mmm = std::max(x->data, std::max(x->c[0]->mmm, x->c[1]->mmm));
+}
 inline void rev(node *x) {if (x != nil) std::swap(x->c[0], x->c[1]), x->isrev ^= 1;}
 inline void pushdown(node *x) {if (x->isrev) rev(x->c[0]), rev(x->c[1]), x->isrev = false;};
 inline void rotate(node *x) {
+    if (isroot(x))
+        puts("------------");
     node *p = x->parent, *z = p->parent;
     int fx = isrson(x), fp = isrson(p);
     if (!isroot(p)) z->c[fp] = x;
@@ -38,6 +46,7 @@ void expose(node *x) {
     for (; x != nil; p = x, x = x->parent) {
         splay(x);
         x->c[1] = p;
+        update(x);
         if (p != nil) p->parent = x;
     }
 }
@@ -58,24 +67,42 @@ node *findATRoot(node *a) {
 }
 inline int getint();
 node buf[MXN];
-int n, m, a, b; char input[11];
+int n, m, a[MXN], b[MXN], c, d; char input[11];
 inline void init();
 int main() {
-    n = getint(); m = getint();
+    n = getint();
     init();
+    for (int i = 1; i < n; ++i) {
+        a[i] = getint(); b[i] =getint();
+    }
+    for (int i = 1; i <= n; ++i) {
+        buf[i].sum = buf[i].mmm = buf[i].data = getint();
+        buf[i].sz = 1;
+        buf[i].isrev = false;
+        buf[i].parent = buf[i].c[0] = buf[i].c[1] = nil;
+    }
+    for (int i = 1; i < n; ++i) link(buf + a[i], buf + b[i]);
+    makeroot(buf + 3);
+    expose(buf + 4);
+    m = getint();
     while (m--) {
         scanf("%s", input);
-        a = getint(); b = getint();
-        switch (input[0]) {
-        case 'Q':
-            if (findATRoot(buf + a) == findATRoot(buf + b)) puts("Yes");
-            else puts("No");
-            break;
-        case 'C':
-            link(buf + a, buf + b);
-            break;
-        case 'D':
-            cut(buf + a, buf + b);
+        c = getint(); d = getint();
+        if (input[1] == 'H') {
+            expose(buf + c);
+            splay(buf + c);
+            buf[c].data = d;
+            update(buf + c);
+        } else if (input[1] == 'M') {
+            makeroot(buf + c);
+            expose(buf + d);
+            splay(buf + d);
+            printf("%d\n", buf[d].mmm);
+        } else {
+            makeroot(buf + c);
+            expose(buf + d);
+            splay(buf + d);
+            printf("%d\n", buf[d].sum);
         }
     }
 }
@@ -90,12 +117,10 @@ int getint() {
 }
 void init() {
     nil = &_nil;
+    nil->sum = 0;
+    nil->data = 0;
+    nil->mmm = -INF;
     nil->sz = 0;
     nil->parent = nil->c[0] = nil->c[1] = nil;
     nil->isrev = false;
-    for (int i = 1; i <= n; ++i) {
-        buf[i].sz = 1;
-        buf[i].isrev = false;
-        buf[i].parent = buf[i].c[0] = buf[i].c[1] = nil;
-    }
 }
